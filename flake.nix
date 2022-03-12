68ae38e46bc3d504dc984add5bc8b1434a29759e
hugosenari <hugosenari@gmail.com>
2022-03-12 04:13:26 -0300

    chore: re index niledb

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = ''Key/Value storage into a fast file-hash'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."niledb-master".url = "path:./master";
+  inputs."niledb-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."niledb-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."niledb-v1_0_0".url = "path:./v1_0_0";
+  inputs."niledb-v1_0_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."niledb-v1_0_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."niledb-v1_1_0".url = "path:./v1_1_0";
+  inputs."niledb-v1_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."niledb-v1_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."niledb-v1_3_0".url = "path:./v1_3_0";
+  inputs."niledb-v1_3_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."niledb-v1_3_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."niledb-v1_3_1".url = "path:./v1_3_1";
+  inputs."niledb-v1_3_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."niledb-v1_3_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ v1_3_0/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''Key/Value storage into a fast file-hash'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-niledb-v1_3_0.flake = false;
+  inputs.src-niledb-v1_3_0.owner = "JeffersonLab";
+  inputs.src-niledb-v1_3_0.ref   = "refs/tags/v1.3.0";
+  inputs.src-niledb-v1_3_0.repo  = "niledb";
+  inputs.src-niledb-v1_3_0.type  = "github";
+  
+  inputs."serializetools".dir   = "nimpkgs/s/serializetools";
+  inputs."serializetools".owner = "riinr";
+  inputs."serializetools".ref   = "flake-pinning";
+  inputs."serializetools".repo  = "flake-nimble";
+  inputs."serializetools".type  = "github";
+  inputs."serializetools".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."serializetools".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-niledb-v1_3_0"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-niledb-v1_3_0";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
